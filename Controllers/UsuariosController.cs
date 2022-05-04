71f441a9b3a3bfea5533fc4054a00a89eb858733
MATEUS VIZOTO <mateus.vizoto@fatec.sp.gov.br>
2022-05-04 10:55:42 -0300

    Aula favorito

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,6 +1,7 @@
 ﻿using AulaRelacionamento.Entidades;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,14 @@ namespace AulaRelacionamento.Controllers
             db = contexto;
         }
 
+        public ActionResult MeusFavoritos(int id)
+        {
+            Models.MeusFavoritosModel model = new Models.MeusFavoritosModel();
+            model.UsuarioId = id;
+            model.todosFilmes = db.FILMES.ToList();
+            model.favoritos = db.USUARIOS_FILMES.Where(a => a.UsuarioId == id).Include(a => a.filme).ToList();
+            return View ();
+        }
         public ActionResult Index()
         {
             return View(db.USUARIOS.ToList());
---
+++ Controllers/UsuariosFilmesController .cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaRelacionamento.Controllers
+{
+    public class UsuarioFilmeController : Controller
+    {
+        private readonly Contexto db;
+
+        public UsuarioFilmeController(Contexto contexto)
+        {
+            db = contexto;
+        }
+        [HttpGet("[controller]/[action]/{usuarioId}/{filmeId}")]
+        public ActionResult Favoritar (int usuarioId, int filmeId)
+        {
+            Entidades.Usuarios_Filmes novoRegistro = new  Entidades.Usuarios_Filmes();
+            novoRegistro.FilmeId = filmeId;
+            novoRegistro.UsuarioId = usuarioId;
+            db.USUARIOS_FILMES.Add(novoRegistro);
+            db.SaveChanges();
+            return Redirect("/Usuarios");
+        }
+        public ActionResult RemoverFavoritos(int id)
+        {
+            db.USUARIOS_FILMES.Remove(db.USUARIOS_FILMES.Find(id));
+            db.SaveChanges();
+            return Redirect( "/Usuarios");
+        }
+    }
+}
